17bdedbc38d0fdf9302372b5e3f04f995997cad3
Kowasz Wojciech <wojciech.kowasz@grupawp.pl>
2019-07-25 13:24:34 +0200

    MySQL database support

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,6 +80,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": os.environ['DB_NAME'],
+#         "USER": os.environ['DB_USER'],
+#         "PASSWORD": os.environ['DB_PASS'],
+#         "HOST": os.environ['DB_HOST'],
+#         "PORT": 3306,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
